e39accbc36d20d127ecff750870027ddc27fccb0
mikeybellissimo <leucha@gmail.com>
2024-12-22 14:09:01 -0500

    Got tough part verified. now to test mobile

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
-from streamlit_webrtc import webrtc_streamer
+import streamlit as st
 
-webrtc_streamer(key="sample")
+enable = st.checkbox("Enable camera")
+picture = st.camera_input("Take a picture", disabled=not enable)
+
+audio_value = st.audio_input("Record a voice message")
+
+if audio_value:
+    st.audio(audio_value)
+
+if picture:
+    st.image(picture)
