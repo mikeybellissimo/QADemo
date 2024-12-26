39e8590e49e58dced1447c96e2e212c3f952dadf
mikeybellissimo <leucha@gmail.com>
2024-12-26 08:40:44 -0500

    back camera

--- create_event.py
+++ create_event.py
@@ -1,13 +1,14 @@
 import streamlit as st
 import tempfile 
 from speech import speech_to_text
+from streamlit_back_camera_input import back_camera_input
 
 def create_event_page():
     def get_modified_raw_description():
         st.session_state.new_event_description_raw = st.session_state["raw_description_text_area"]
         
 
-    recent_picture = st.camera_input(label="Take a picture of an issue", label_visibility="hidden", key=f"camera_{st.session_state.camera_clear_hack}")
+    recent_picture = back_camera_input(label="Take a picture of an issue", label_visibility="hidden", key=f"camera_{st.session_state.camera_clear_hack}")
 
     audio = st.audio_input("Describe the Issue", key=f"audio_{st.session_state.audio_input_hack}")
     
